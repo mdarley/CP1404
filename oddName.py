b3e6e046a8437e5b8a704366fc7e8fa6d8182423
mdarley <matthew.darley@my.jcu.edu.au>
2017-08-17 09:24:29 +1000

    pint second letter in name

--- oddName.py
+++ oddName.py
@@ -1,3 +1,9 @@
 """
 Matthew Darley
 """
+
+name = str(input("Enter your name: "))
+while name == '':
+    print("Please enter your name")
+    name = str(input("Enter your name: "))
+print(name[1::2])
