ba07085572f2c75be0629b1887d40890d6ea4aa0
mdarley <matthew.darley@my.jcu.edu.au>
2017-08-17 09:58:14 +1000

    add functions for printing name in steps

--- oddName.py
+++ oddName.py
@@ -2,8 +2,22 @@
 Matthew Darley
 """
 
-name = str(input("Enter your name: "))
-while name == '':
-    print("Please enter your name")
+
+def main():
+    name = get_name()
+    print_steps(name, 2)
+
+
+def get_name():
     name = str(input("Enter your name: "))
-print(name[1::2])
+    while name == '':
+        print("Invalid name")
+        name = str(input("Enter your name: "))
+    return name
+
+
+def print_steps(name,step=2):
+    print(name[1::step])
+
+
+main()
