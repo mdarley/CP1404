e15133e7f409edd8b21622ccd0bf5f78bce4f648
mdarley <matthew.darley@my.jcu.edu.au>
2017-08-17 10:25:45 +1000

    copying broken_score and temperatures into git

--- broken_score.py
+++ broken_score.py
@@ -3,12 +3,19 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score < 50:
-    print("Bad score")
-elif score < 90:
-    print("passable")
-else:
-    print("Excellent")
+def main():
+    score = float(input("Enter score: "))
+    print(score_calc(score))
+
+def score_calc(score):
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score < 50:
+        result = "Bad score"
+    elif score < 90:
+        result = "passable"
+    else:
+        result ="Excellent"
+    return result
+
+main()
